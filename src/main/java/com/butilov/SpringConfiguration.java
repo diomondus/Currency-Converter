2df17c2a8a465fcc7107e198fa2556ae040cb7fb
Dmitry Butilov <acebutilov@gmail.com>
2018-02-10 09:52:11 +0300

    Удаляет лишний bean

--- src/main/java/com/butilov/SpringConfiguration.java
+++ src/main/java/com/butilov/SpringConfiguration.java
@@ -5,16 +5,11 @@ package com.butilov;
  * on 10.02.18.
  */
 
-import com.butilov.services.RatesDeserializerService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.butilov")
 public class SpringConfiguration {
-    @Bean
-    public RatesDeserializerService getRatesDeserializerBean() {
-        return new RatesDeserializerService();
-    }
+
 }
